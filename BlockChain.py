01cf7cc24cb8fc57ff96b6f65739272af154668c
Hani J <hanijandali@gmail.com>
2022-06-12 16:15:40 -0700

    API integration of Blockchain

--- BlockChain.py
+++ BlockChain.py
@@ -59,6 +59,63 @@ class Blockchain(object):
         #Calls and returns the last block of the chain
         return self.chain[-1]
 
+#---------------------- API Integration ----------------------#
+
+# Creating the app node
+app = Flask(__name__)
+node_identifier = str(uuid4()).replace('-','')
+# Initializing blockchain
+blockchain = Blockchain()
+@app.route('/mine', methods=['GET'])
+def mine():
+   #Proof of Work Algorithm
+   last_block = blockchain.last_block
+   last_proof = last_block['proof']
+   proof = blockchain.proof_of_work(last_proof)
+
+   #Miner Reward for Contribution
+   blockchain.new_transaction(
+       sender="0",
+       recipient = node_identifier,
+       amount = 1,
+   )
+
+   #Creation of New Block and Added to Chain
+   previous_hash = blockchain.hash(last_block)
+   block = blockchain.new_block(proof, previous_hash)
+   response = {
+       'message': 'The new block has been forged',
+       'index': block['index'],
+       'transactions': block['transactions'],
+       'proof': block['proof'],
+       'previous_hash' : block['previous_hash']
+   }
+   return jsonify(response), 200
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+    #Checking if required data is there
+    required=['sender','recipient','amount']
+    if not all(k in values for k in required):
+        return 'Missing Values', 400
+    
+    #Creating new transaction
+    index = blockchian.new_transaction(values['sender'], values['recipient', values['amount']])
+    response = {'message': f'TRansaction is scheduled to be added to Block No. {index}'}
+    return jsonify(response), 201
+
+@app.router('/chain', methods=['GET'])
+def full_chain():
+   response = {
+       'chain' : blockchain.chain,
+       'length' : len(blockchain.chain)
+   }
+   return jsonify(response), 200
+
+if __name__ == '__main__':
+   app.run(host="0.0.0.0", port=5000)
+
 
 
 
