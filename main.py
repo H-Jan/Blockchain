bd85ebc69c04b681ac50e18c2ba509a12fa0cfa6
Hani J <hanijandali@gmail.com>
2022-06-12 15:49:57 -0700

    Creation of blockchain class with basic features for blockchain viability

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-main.py
+
+#Blockchain class
+class Blockchain(object):
+    def __init__(self):
+        #Initial Chain and Trasnaction list
+        self.chain=[]
+        self.current_transactions=[]
+
+    def new_block(self):
+        #Block Function creates new blocks and adds to existing chain
+        pass
+
+    def new_transaction(self):
+        #Adds a new transaction to current_transactions
+        pass
+
+    @staticmethod
+    def hash(block):
+        #Hashing blocks
+    
+    @property
+    def last_block(self):
+        #Calls and returns the last block of the chain
+        pass
+
+    
+
+
+
